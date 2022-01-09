90e9cf255fe7e12268de7f37120fcc621f772485
Lĩnh Nguyễn Hoàng <alinh1803@gmail.com>
2022-01-09 14:01:00 +0700

    updated

--- main_window_ui.py
+++ main_window_ui.py
@@ -1,13 +1,21 @@
 # coding=utf-8
 import sys
 import pefile
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 import codecs
+import os
 
 
 class Window(QtGui.QMainWindow):
     def __init__(self):
         super(Window, self).__init__()
+        self.section = QtGui.QTextEdit()
+        self.section_header = QtGui.QTextEdit()
+        self.file_header = QtGui.QTextEdit()
+        self.signature = QtGui.QTextEdit()
+        self.optional_header = QtGui.QTextEdit()
+        self.dos_header = QtGui.QTextEdit()
+        self.binary_value = QtGui.QTextEdit()
         self.mdi = QtGui.QMdiArea()
         self.setCentralWidget(self.mdi)
         # print("abcd")
@@ -47,17 +55,25 @@ class Window(QtGui.QMainWindow):
         fileMenu.addAction(exit_app)
 
         self.toolbar()
+        # self.home()
+
+        # self.show()
         self.create_sub_window(1925, 2325)
 
     def create_sub_window(self, width, height):
         """Display SubWindow"""
         sub_window = QtGui.QMdiSubWindow()
-        self.edit = QtGui.QTextEdit()
-        self.edit.setReadOnly(True)
-        sub_window.setWidget(self.edit)
         self.mdi.addSubWindow(sub_window)
         sub_window.setMinimumSize(width, height)
         sub_window.show()
+        self.create_list_button()
+
+    def create_list_button(self):
+        program = QtGui.QPushButton("Program", self)
+
+        program.resize(program.minimumSizeHint())
+        program.move(6, 102)
+        program.show()
 
     def toolbar(self):
         """Display toolbar"""
@@ -80,25 +96,26 @@ class Window(QtGui.QMainWindow):
             self, 'Open File', '', 'All Files(*.exe*)')
         pe = pefile.PE(name, fast_load=True)
         pe.full_load()
-        self.editor()
-        self.textEdit.setReadOnly(True)
-        self.read_image_dos_header(pe)
-        self.read_optional_header(pe)
-        self.read_image_section_header(pe)
-        self.read_sections(pe)
-        self.read_signature(pe)
-        self.read_image_file_header(pe)
+        # self.read_binary(name)
+        # self.editor()
+        # self.textEdit.setReadOnly(True)
+        # self.read_image_dos_header(pe)
+        # self.read_optional_header(pe)
+        # self.read_image_section_header(pe)
+        # self.read_sections(pe)
+        # self.read_signature(pe)
+        # self.read_image_file_header(pe)
 
     def read_binary(self, name):
         """Read Binary File"""
         s = codecs.open(name, 'rb', 'mbcs').read()
-        self.editor()
-        self.textEdit.setReadOnly(True)
-        self.textEdit.setPlainText(s)
+        self.binary_value.setReadOnly(True)
+        # self.editor()
+        # self.textEdit.setReadOnly(True)
+        self.binary_value.setPlainText(s)
 
     def read_image_dos_header(self, pe):
         """Read Image Dos Header"""
-        self.dos_header = QtGui.QTextEdit()
         self.dos_header.setReadOnly(True)
         for field in pe.DOS_HEADER.dump():
             self.setCentralWidget(self.dos_header)
@@ -106,7 +123,6 @@ class Window(QtGui.QMainWindow):
 
     def read_optional_header(self, pe):
         """Read Optional Header"""
-        self.optional_header = QtGui.QTextEdit()
         self.optional_header.setReadOnly(True)
         for data_dir in pe.OPTIONAL_HEADER.dump():
             self.setCentralWidget(self.optional_header)
@@ -120,14 +136,12 @@ class Window(QtGui.QMainWindow):
 
     def read_signature(self, pe):
         """Read NT_HEADERS Signature"""
-        self.signature = QtGui.QTextEdit()
         for field in pe.NT_HEADERS.dump():
             self.setCentralWidget(self.signature)
             self.signature.append(field)
 
     def read_image_file_header(self, pe):
         """Read NT_HEADERS FILE_HEADER"""
-        self.file_header = QtGui.QTextEdit()
         self.file_header.setReadOnly(True)
         for field in pe.FILE_HEADER.dump():
             self.setCentralWidget(self.file_header)
@@ -135,7 +149,6 @@ class Window(QtGui.QMainWindow):
 
     def read_image_section_header(self, pe):
         """Read Image Section Header"""
-        self.section_header = QtGui.QTextEdit()
         self.section_header.setReadOnly(True)
         self.setCentralWidget(self.section_header)
         self.section_header.setPlainText(
@@ -143,7 +156,6 @@ class Window(QtGui.QMainWindow):
 
     def read_sections(self, pe):
         """Read Section Header"""
-        self.section = QtGui.QTextEdit()
         self.section.setReadOnly(True)
         for section in pe.sections:
             self.setCentralWidget(self.section)
@@ -157,7 +169,7 @@ class Window(QtGui.QMainWindow):
 
     def close_application(self):
         """Close Application"""
-        sys.exit(0)
+        sys.exit()
 
     def editor(self):
         """Edit data"""
