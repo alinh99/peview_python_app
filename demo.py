c543d6073e59e93deb2b105b42a110d3ba2c2f92
Lĩnh Nguyễn Hoàng <alinh1803@gmail.com>
2022-01-18 08:45:58 +0700

    updated pFile column code

--- demo.py
+++ demo.py
@@ -9,8 +9,24 @@ pe_data = mmap.mmap(fd.fileno(), 0, access=mmap.ACCESS_READ)
 print(pe_data)
 
 # Parse the data contained in the buffer
-pe = pefile.PE(data=pe_data)
-lst_dos_header = []
-for val_dos_header in pe.DOS_HEADER.dump():
-    lst_dos_header.append(val_dos_header)
-print(pe.DOS_HEADER.__keys__)
+# pe = pefile.PE(data=pe_data)
+# lst_dos_header = []
+# for val_dos_header in pe.DOS_HEADER.dump():
+#     lst_dos_header.append(val_dos_header)
+# print(pe.dword_align(None, pe.OPTIONAL_HEADER))
+
+pe = pefile.PE(exe_path)
+# print "PE Signature: " + hex(pe.VS_FIXEDFILEINFO.Signature)
+print "Image Base: " + hex(pe.OPTIONAL_HEADER.ImageBase)
+print "Address of EntryPoint: " + hex(pe.OPTIONAL_HEADER.AddressOfEntryPoint)
+print "RVA Number and Size: " + hex(pe.OPTIONAL_HEADER.NumberOfRvaAndSizes)
+print "Number of Sections within PE: " + hex(pe.FILE_HEADER.NumberOfSections)
+print pe.DOS_HEADER.dump_dict()
+# print hex(pe.DOS_HEADER.dump_dict()['CheckSum']['FileOffset'])
+
+# for entry in pe.OPTIONAL_HEADER.DATA_DIRECTORY:
+#     print entry.name + "\n|\n|---- Size : " + str(entry.Size) + "\n|\n|---- VirutalAddress : " + hex(entry.VirtualAddress) + '\n'
+lst = []
+for section in pe.sections:
+    lst.append(section)
+print(lst[0])
